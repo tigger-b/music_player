8fe36df520c261bcf01a23cc6035d1322ccbbb39
Tadhg Bryant <tadhgbryant@Tadhgs-MacBook-Air.local>
2025-10-29 15:47:37 +1100

    add music files, add album artwork, updated code to add stop button

--- music_player.rb
+++ music_player.rb
@@ -38,12 +38,10 @@ def read_albums(filename)
   File.open(filename, 'r') { |f| Array.new(f.gets.to_i) { read_album(f) } }
 end
 
-# ============================
-# ===== MUSIC PLAYER WINDOW ==
-# ============================
+#music player window
 class MusicPlayer < Gosu::Window
   def initialize(albums)
-    # ===== WINDOW SETTINGS =====
+    # window settings
     @win_width = 800
     @win_height = 600
     @album_size = 200
@@ -53,30 +51,36 @@ class MusicPlayer < Gosu::Window
     super(@win_width, @win_height)
     self.caption = "🎵 Ruby GUI Music Player"
 
-    # ===== INSTANCE VARIABLES =====
+    
     @albums = albums
     @album_positions = []
     @album_hover = []
     @selected_album_index = nil
     @current_song = nil
-    @now_playing = ""       # Name of track currently highlighted/playing
+    @now_playing = ""       #name of track currently playing
 
-    # ===== FONTS =====
+    
     @font = Gosu::Font.new(20)
     @small_font = Gosu::Font.new(16)
 
-    # ===== LOAD ALBUM IMAGES =====
+    @stop_button_hover = false
+    @stop_button_x = 20
+    @stop_button_y = @win_height - 60
+    @stop_button_width = 120
+    @stop_button_height = 35
+
+    #images
     @artworks = albums.map do |a|
       path = File.join("media", a.artwork)
       if File.exist?(path)
         Gosu::Image.new(path)
       else
-        # red square placeholder if image not found
+        #image not found case
         Gosu::Image.from_blob(@album_size, @album_size, "\xff\x00\x00\x00" * @album_size * @album_size)
       end
     end
 
-    # ===== CALCULATE ALBUM POSITIONS FOR GRID =====
+    #grid for albums
     @albums.each_with_index do |_, i|
       col = i % 2
       row = i / 2
@@ -87,9 +91,7 @@ class MusicPlayer < Gosu::Window
     end
   end
 
-  # ============================
-  # ===== DRAW METHODS =========
-  # ============================
+ 
   def draw
     Gosu.draw_rect(0, 0, @win_width, @win_height, Gosu::Color.new(30, 30, 30), ZOrder::BACKGROUND)
     draw_albums
@@ -102,22 +104,22 @@ class MusicPlayer < Gosu::Window
       x, y = @album_positions[i]
       img = @artworks[i]
 
-      # Scale image to fit hover/click box
+      
       scale_x = @album_size.to_f / img.width
       scale_y = @album_size.to_f / img.height
       img.draw(x, y, ZOrder::MIDDLE, scale_x, scale_y)
 
-      # Draw hover outline
+      
       if @album_hover[i]
         outline_thickness = 4
         # Top
-        Gosu.draw_rect(x, y, @album_size, outline_thickness, Gosu::Color::YELLOW, ZOrder::TOP)
+        Gosu.draw_rect(x, y, @album_size, outline_thickness, Gosu::Color.argb(0xff_ff0000), ZOrder::TOP)
         # Bottom
-        Gosu.draw_rect(x, y + @album_size - outline_thickness, @album_size, outline_thickness, Gosu::Color::YELLOW, ZOrder::TOP)
+        Gosu.draw_rect(x, y + @album_size - outline_thickness, @album_size, outline_thickness, Gosu::Color.argb(0xff_ff0000), ZOrder::TOP)
         # Left
-        Gosu.draw_rect(x, y, outline_thickness, @album_size, Gosu::Color::YELLOW, ZOrder::TOP)
+        Gosu.draw_rect(x, y, outline_thickness, @album_size, Gosu::Color.argb(0xff_ff0000), ZOrder::TOP)
         # Right
-        Gosu.draw_rect(x + @album_size - outline_thickness, y, outline_thickness, @album_size, Gosu::Color::YELLOW, ZOrder::TOP)
+        Gosu.draw_rect(x + @album_size - outline_thickness, y, outline_thickness, @album_size, Gosu::Color.argb(0xff_ff0000), ZOrder::TOP)
       end
     end
   end
@@ -128,12 +130,12 @@ class MusicPlayer < Gosu::Window
     x_start = 500
     y_start = 20
 
-    @font.draw_text("Tracks from #{album.title}", x_start, y_start, ZOrder::TOP, 1.2, 1.2, Gosu::Color::YELLOW)
+    @font.draw_text("Tracks from #{album.title}", x_start, y_start, ZOrder::TOP, 1.2, 1.2, Gosu::Color::WHITE)
 
     album.tracks.each_with_index do |track, i|
       y = y_start + 40 + i * 30
       color = if track.name == @now_playing
-                Gosu::Color::YELLOW   # Highlight selected track
+                Gosu::Color.argb(0xff_ff0000) #highight for track  
               else
                 Gosu::Color::WHITE
               end
@@ -142,23 +144,36 @@ class MusicPlayer < Gosu::Window
   end
 
   def draw_now_playing
-    @font.draw_text("Now Playing: #{@now_playing}", 20, @win_height - 40, ZOrder::TOP, 1, 1, Gosu::Color::CYAN)
+    @font.draw_text("Now Playing: #{@now_playing}", 20, @win_height - 100, ZOrder::TOP, 1, 1, Gosu::Color::WHITE)
+    #stop button
+    color = @stop_button_hover ? Gosu::Color::RED : Gosu::Color::WHITE
+    text_color = @stop_button_hover ? Gosu::Color::WHITE : Gosu::Color::BLACK
+
+    Gosu.draw_rect(@stop_button_x, @stop_button_y, @stop_button_width, @stop_button_height, color, ZOrder::MIDDLE)
+    @font.draw_text("⏹ Stop", @stop_button_x + 35, @stop_button_y + 8, ZOrder::TOP, 1, 1, text_color)
+   
+    
   end
 
-  # ============================
-  # ===== EVENT METHODS =======
-  # ============================
+  
   def needs_cursor?; true; end
 
   def update
     @album_positions.each_with_index do |(x, y), i|
       @album_hover[i] = mouse_x.between?(x, x + @album_size) && mouse_y.between?(y, y + @album_size)
+    @stop_button_hover = mouse_x.between?(@stop_button_x, @stop_button_x + @stop_button_width) &&
+      mouse_y.between?(@stop_button_y, @stop_button_y + @stop_button_height)
     end
   end
 
   def button_down(id)
     if id == Gosu::MsLeft
-      # Album click
+      #stop button click
+      if @stop_button_hover
+        stop_music
+        return
+      end
+      #album click
       @album_positions.each_with_index do |(x, y), i|
         if mouse_x.between?(x, x + @album_size) && mouse_y.between?(y, y + @album_size)
           @selected_album_index = i
@@ -166,7 +181,7 @@ class MusicPlayer < Gosu::Window
         end
       end
 
-      # Track click
+      #track click
       return unless @selected_album_index
       album = @albums[@selected_album_index]
       x_start = 500
@@ -175,7 +190,7 @@ class MusicPlayer < Gosu::Window
         y = y_start + i * 30
         if mouse_x.between?(x_start, x_start + 300) && mouse_y.between?(y, y + 20)
           puts "Clicked #{track.file}"
-          @now_playing = track.name     # Set highlighted track
+          @now_playing = track.name     #for highlighted track
           play_track(track)
         end
       end
@@ -190,6 +205,13 @@ class MusicPlayer < Gosu::Window
       puts "Loading #{track.file}"
       @current_song = Gosu::Song.new(track.file)
       @current_song.play(false)
+    end
+  end
+
+  def stop_music
+    if @current_song
+      @current_song.stop
+      @now_playing = ""
     end
   end
 end
